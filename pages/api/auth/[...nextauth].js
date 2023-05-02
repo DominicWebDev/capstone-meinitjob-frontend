92af0cb7e6760e84555d8771c474f8b0da63054a
Dominic Matte <mattedominic@gmx.de>
2023-05-03 01:24:41 +0200

    fix: vercel deployment sequelize

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,9 +2,11 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import SequelizeAdapter, { models } from "@next-auth/sequelize-adapter";
 import Sequelize, { DataTypes } from "sequelize";
+import pg from "pg";
 /* import User from "../models/User"; */
 
 const sequelize = new Sequelize(`${process.env.CONNECTION_STRING}`, {
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       require: true,
@@ -19,153 +21,20 @@ sequelize.sync({ alter: true }); */
 
 console.log("HERE ARE THE models", models);
 
-/* const User = sequelize.define(
-  "user",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    first_name: {
-      type: DataTypes.STRING,
-    },
-    last_name: {
-      type: DataTypes.STRING,
-    },
-    name: {
-      type: DataTypes.STRING,
-    },
-    email: {
-      type: DataTypes.STRING,
-    },
-    image: {
-      type: DataTypes.STRING,
-    },
-  },
-  { timestamps: true, createdAt: "created_at", updatedAt: "updated_at" }
-); */
-
-/* const Account = sequelize.define("Account", {
-  provider: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  providerAccountId: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  refresh_token: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  accessToken: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  accessTokenExpires: {
-    type: DataTypes.DATE,
-    allowNull: true,
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-}); */
-
-/* const Account = sequelize.define("account", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: "users",
-      key: "id",
-    },
-    onUpdate: "CASCADE",
-    onDelete: "RESTRICT",
-  },
-}); */
-
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      /* async authorize(credentials, req) {
-        const { tokens } = credentials;
-        const profile = await this.fetchProfile(tokens.accessToken);
-        console.log("I AM STARTING THE AUTHORIZE OF GITHUB NOW! credentials");
-        console.log("----------credentials", credentials);
-        console.log("----------credentials", tokens);
-        console.log("----------credentials", profile);
-
-        if (!profile) {
-          throw new Error("GitHub-Profile nicht verfügbar");
-        }
-
-        let user = await UserModel.findOne({
-          where: { oauth_user_id: profile.id },
-        });
-
-        console.log("FOUND USER: ", user);
-
-        if (!user) {
-          user = await UserModel.create({
-            oauth_user_id: profile.id,
-            name: profile.name,
-            email: profile.email,
-            image: profile.image,
-          });
-        }
-
-        console.log("USER IS NOW (AFTER POTENTIAL CREATION): ", user);
-
-        return {
-          id: user.oauth_user_id,
-          name: user.name,
-          email: user.email,
-          image: user.image,
-        };
-      }, */
     }),
   ],
   adapter: SequelizeAdapter(sequelize, {
     models: {
-      User: sequelize.define(
-        "oauthuser",
-        { ...models.User }
-        /* id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-        },
-        oauth_user_id: {
-          type: DataTypes.UUID,
-          defaultValue: DataTypes.UUIDV4,
-          unique: true,
-        },
-        first_name: DataTypes.STRING,
-        last_name: DataTypes.STRING,
-        pref_remote: DataTypes.BOOLEAN,
-        pref_company_size: DataTypes.INTEGER,
-        pref_sector: DataTypes.STRING,
-        description: DataTypes.STRING, */
-        /*   }  */
-        /* { timestamps: true, created: "created_at", updated: "updated_at" } */
-      ),
+      User: sequelize.define("oauthuser", { ...models.User }),
       Account: sequelize.define("account", {
         ...models.Account,
-        /*    id: {
-          type: DataTypes.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-        }, */
+
         userId: {
           type: DataTypes.UUID,
           allowNull: false,
@@ -193,24 +62,8 @@ export const authOptions = {
     },
   }),
 
-  /* adapter: SequelizeAdapter(sequelize, {
-    models: {
-     
-      User,
-    },
-  }), */
   debug: true,
   callbacks: {
-    /*   async signIn(user, account, profile) {
-      console.log("CALLBBACK SIGNIN user", user)
-      console.log("CALLBBACK SIGNIN user", user)
-      console.log("CALLBBACK SIGNIN user", user)
-
-      if (account.provider === "github") {
-        user.oauth_user_id = profile.id;
-      }
-      return true;
-    }, */
     async signIn(callback) {
       console.log("CALLBBACK SIGNIN callback", callback);
       if (callback.account.provider === "github") {
@@ -270,33 +123,6 @@ export const authOptions = {
           });
 
         console.log(user, "__________________- Das ist das Ende");
-
-        /*    const adapter = this.adapter;
-        const UserModel = adapter.models.User;
-
-        let dbUser = await UserModel.findOne({
-          where: { oauth_user_id: callback.profile.id },
-        });
-
-        console.log("MY FOUND USER IS dbUser: ", dbUser);
-
-        if (!dbUser) {
-          console.log("I AM CREATING THE USER");
-          dbUser = await UserModel.create({
-            oauth_user_id: callback.profile.id,
-            name: callback.user.name,
-            email: callback.user.email,
-            image: callback.user.image,
-          });
-        }
-
-        console.log("MY USER IS NOW dbUser: ", dbUser); */
-
-        // Aktualisiere das user-Objekt mit den gefundenen oder erstellten Benutzerdaten
-        /*      callback.user.id = dbUser.oauth_user_id;
-        callback.user.name = dbUser.name;
-        callback.user.email = dbUser.email;
-        callback.user.image = dbUser.image; */
       }
       return true;
     },
